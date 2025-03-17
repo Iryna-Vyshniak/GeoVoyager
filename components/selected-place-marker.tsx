70635fe9addec3f27fb0bca31ddc21dc09b50b2c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-03-17 21:45:08 +0200

    feat: Refactor SelectedPlaceMarker to use 3D text and pin model with hover effects

--- components/selected-place-marker.tsx
+++ components/selected-place-marker.tsx
@@ -1,50 +1,62 @@
 'use client';
 
-import { forwardRef, useRef } from 'react';
-import { Html } from '@react-three/drei';
+import { forwardRef, useEffect, useMemo, useRef, useState } from 'react';
+import { Billboard, Text3D, useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { animated, useSpring } from '@react-spring/three';
-import { FaMapMarkerAlt } from 'react-icons/fa';
 
-import { Group } from 'three';
+import { BufferGeometry, Group, Mesh, MeshPhysicalMaterial, MeshStandardMaterial } from 'three';
 
 import { lon2xyz } from '@/lib/utils';
 import { SelectedPlaceMarkerProps } from '@/shared/shared.types';
 
 const SelectedPlaceMarker = forwardRef<Group, SelectedPlaceMarkerProps>(
-  ({ selectedPlace, isVisible, setOccluded }, ref) => {
+  ({ selectedPlace, isVisible }, ref) => {
     const markerRef = ref as React.MutableRefObject<Group | null>;
-    const htmlGroupRef = useRef<Group>(null);
-
-    const pulseAnimation = useSpring({
-      scale: 1.5,
-      from: { scale: 0.25 },
-      loop: { reverse: true },
-      config: { tension: 100, friction: 20 },
-    });
-
-    // Автоматичне орієнтування HTML на камеру (billboard-ефект)
-    useFrame((state) => {
-      if (htmlGroupRef.current) {
-        // Орієнтуємо групу на камеру, щоб текст завжди був спрямований до глядача
-        htmlGroupRef.current.quaternion.copy(state.camera.quaternion);
-        // Додаємо невелике вертикальне вирівнювання, щоб текст був горизонтальним
-        htmlGroupRef.current.rotation.x = 0; // Фіксуємо горизонтальне положення, щоб текст був стабільним
-        htmlGroupRef.current.rotation.z = 0; // Фіксуємо поворот навколо Z -  без повороту навколо Z
-
-        // Синхронізуємо позицію HTML із міткою
-        if (markerRef.current) {
-          const markerPosition = lon2xyz(...selectedPlace.coordinates, 1); // Використовуємо lon2xyz для позиції мітки
-          htmlGroupRef.current.position.set(
-            markerPosition.x, // Зсув вліво по осі X
-            markerPosition.y, // Зсув вгору по осі Y над міткою
-            markerPosition.z // Без зсуву по Z
-          );
-
-          htmlGroupRef.current.rotation.set(0, 0, 0);
-        }
+    const textRef = useRef<Mesh<BufferGeometry, MeshStandardMaterial>>(null);
+    const pinRef = useRef<Mesh>(null);
+  
+     const { scene } = useGLTF('/assets/textures/mappin.glb');
+      const gltfScene = useMemo(() => scene.clone(), [scene]);
+      const [hovered, setHovered] = useState<boolean>(false);
+      
+      useEffect(() => {
+         if (hovered) { 
+          document.body.style.cursor = 'pointer';
+        };
+        return () => {
+            document.body.style.cursor = 'auto';
+          };
+      }, [hovered]);
+      
+    useFrame(() => {
+      if (textRef.current && textRef.current.material) {
+        const material = textRef.current.material as MeshPhysicalMaterial;
+        material.color.set(hovered ? '#7b00ff' : '#ffffff'); 
+        material.emissive.set(hovered ? '#0d00ff' : '#ffffff'); // Глибший синій світіння
+        material.emissiveIntensity = hovered ? 0.8 : 0.3;
       }
     });
+
+    useFrame(({ clock}) => {
+      if(pinRef.current) { pinRef.current.rotation.y = clock.elapsedTime}
+     
+    })
+
+    const over = (e) => {
+      e.stopPropagation();
+      setHovered(true);
+    };
+
+    const out = () => setHovered(false);
+
+  const pulseAnimation = useSpring({
+    scale: 1,
+    from: { scale: 0.25 },
+    loop: { reverse: true },
+    config: { tension: 100, friction: 20 },
+  });
+ 
 
     return (
       <group
@@ -69,39 +81,49 @@ const SelectedPlaceMarker = forwardRef<Group, SelectedPlaceMarkerProps>(
           />
         </animated.mesh>
 
-        <group ref={htmlGroupRef}>
-          {' '}
-          <Html
-            as='div'
-            center
-            occlude='raycast'
-            onOcclude={(occluded: boolean) => setOccluded(occluded)}
-            style={{
-              position: 'relative',
-              transition: 'all 0.2s',
-              opacity: isVisible ? 1 : 0,
-              transform: `scale(${isVisible ? 1 : 0.25})`,
-            }}
-            distanceFactor={10} // Відстань, на якій HTML видимий
-          >
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                transform: 'scale(1)',
-                background: 'transparent',
-              }}
+        <Billboard position={lon2xyz(...selectedPlace.coordinates, 0.5).toArray()}>
+        <group>
+          <mesh ref={pinRef}  position={[-0.05, 0, 0]}>
+            <primitive object={gltfScene} scale={0.02}/>
+          </mesh>
+        
+          <mesh position={[0, -0.04, 0]}>
+            <Text3D
+              ref={textRef}
+              font="/fonts/PlayfairDisplay.json"
+              size={0.08} 
+             height={0.02} 
+              curveSegments={16}
+              bevelEnabled
+              bevelThickness={0.003}
+              bevelSize={0.002}
+              bevelOffset={0}
+              bevelSegments={8}
+              onPointerOver={over}
+              onPointerOut={out}
+            
+             
             >
-              <FaMapMarkerAlt color='#ff0000' className='text-xs drop-shadow-sm' />
-              <h3 className='text-primary text-xs bg-transparent'>{selectedPlace.name}</h3>
-            </div>
-          </Html>
+              {selectedPlace.name}
+              <meshPhysicalMaterial
+                color="#ffffff"
+                emissive="#7b00ff"
+                emissiveIntensity={0.3}
+                metalness={0.5}
+                roughness={0.4}
+                clearcoat={0.8} 
+                reflectivity={0.8}
+              />
+            </Text3D>
+          </mesh>
         </group>
+          </Billboard>
       </group>
     );
   }
 );
+
+useGLTF.preload('/assets/textures/mappin.glb')
 
 SelectedPlaceMarker.displayName = 'SelectedPlaceMarker';
 
