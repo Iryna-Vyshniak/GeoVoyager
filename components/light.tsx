2f2aa30aa8f5204326eedc49eb67fcf663106ee1
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-03-18 20:54:38 +0200

    feat: Adjust light component properties for improved shadow quality and positioning

--- components/light.tsx
+++ components/light.tsx
@@ -1,42 +1,28 @@
 'use client';
 
 import { useRef } from 'react';
-import { easing } from 'maath';
 import { Group, Object3DEventMap } from 'three';
-import { useFrame } from '@react-three/fiber';
 
-// Джерело світла, яке освітлює об'єкти в сцені
 function Light() {
-  const ref = useRef<Group<Object3DEventMap> | null>(null); // Створюємо посилання на групу об'єктів Three.js
-
-  useFrame((state, delta) => {
-    if (ref.current) {
-      easing.dampE(
-        ref.current.rotation,
-        [(state.pointer.y * Math.PI) / 50, (state.pointer.x * Math.PI) / 20, 0],
-        0.2, // Час згасання (чим менше, тим плавніше)
-        delta
-      );
-    }
-  });
+  const ref = useRef<Group<Object3DEventMap> | null>(null);
 
   return (
     <group ref={ref}>
       <directionalLight
         intensity={5}
-        position={[5, 5, -8]}
+        position={[5, 0, 0]} // Світло справа
         castShadow
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
-        shadow-camera-left={-2}
-        shadow-camera-right={2}
-        shadow-camera-top={-2}
-        shadow-camera-bottom={2}
+        shadow-mapSize-width={4096}
+        shadow-mapSize-height={4096}
+        shadow-camera-left={-5}
+        shadow-camera-right={5}
+        shadow-camera-top={5}
+        shadow-camera-bottom={-5}
         shadow-camera-near={0.1}
-        shadow-camera-far={7}
-        shadow-bias={-0.001}
+        shadow-camera-far={15}
+        shadow-bias={-0.0005}
       >
-        <orthographicCamera attach='shadow-camera' args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]} />
+        <orthographicCamera attach="shadow-camera" args={[-10, 10, 10, -10, 0.1, 20]} />
       </directionalLight>
     </group>
   );
